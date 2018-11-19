34023e820bae12b5e73f1f3df96380db46440779
Sanghyeok Park <pps0209@gmail.com>
2018-11-19 17:38:31 +0000

    implement gauss-seidel method

---
+++ src/gauss_seidel_method.hpp
@@ -0,0 +1,36 @@
+#ifndef SRC_GAUSS_SEIDEL_METHOD_HPP_
+#define SRC_GAUSS_SEIDEL_METHOD_HPP_
+
+#include<vector>
+#include"helper.hpp"
+
+// Assume that A is square.
+template<typename T>
+std::vector<T> jacobi_method(
+        const std::vector<std::vector<T>>& A,
+        std::vector<T> b,
+        T TOL) {
+    const int N = b.size();
+
+    // Initial guessing: zero vector.
+    std::vector<T> x(N);
+    while (1) {
+        std::vector<T> s = b;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                s[i] -= A[i][j]*s[j];
+            }
+            for (int j = i+1; j < N; j++) {
+                s[i] -= A[i][j]*x[j];
+            }
+        }
+
+        for (int i = 0; i < N; i++) s[i] /= A[i][i];
+        T delta = 0;
+        for (int i = 0; i < N; i++) delta += (x[i]-s[i])*(x[i]-s[i]);
+        if (delta < TOL * TOL) return s;
+        x = s;
+    }
+}
+
+#endif  // SRC_GAUSS_SEIDEL_METHOD_HPP_
